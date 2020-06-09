ef6d99ae77d655346302079fe567a6f818ef8486
florin-mavroian <f-mavroian@list.co.jp>
2020-06-09 17:21:28 +0900

    implemented webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = (env) => {
+    return {
+      entry: './src/App.js',
+      output: {
+          filename: 'bundle.js',
+          path: path.join(__dirname, 'public')
+      },
+      module: {
+        rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+        },
+        {
+          use: [{
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+             publicPath: path.join(__dirname, 'public')
+            },
+           }, 'css-loader'
+           ],
+          test: /\.css/
+        }]
+      },
+      mode: env ? 'production' : 'development',
+      devServer: {
+        contentBase: path.join(__dirname, 'public')
+      },
+      plugins: [
+        new MiniCssExtractPlugin({
+        filename: 'main.css',
+        chunkFilename: 'main.css',
+       })]
+    }
+};
