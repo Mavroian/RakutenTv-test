ebfa9a9a14d6756dff7c6512faa17a2cad5b17db
florin-mavroian <f-mavroian@list.co.jp>
2020-06-10 21:33:11 +0900

    loading jsx

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,32 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = (env) => {
     return {
-      entry: './src/App.js',
+      entry: './src/App.jsx',
       output: {
           filename: 'bundle.js',
           path: path.join(__dirname, 'public')
       },
       module: {
         rules: [{
-        loader: 'babel-loader',
-        test: /\.js$/,
-        exclude: /node_modules/
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          resolve: {
+            extensions: [".js", ".jsx"]
+          },
+          use: {
+            loader: "babel-loader"
+          }
+        },
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
         },
         {
           use: [{
